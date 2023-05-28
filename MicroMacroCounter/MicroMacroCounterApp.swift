a4b8a2ed61e120dcb0d89d1afd3ccc478d9e12b6
indianahuey <indianahuey@gmail.com>
2023-05-27 17:41:51 -0700

    Initial Commit

---
+++ MicroMacroCounter/MicroMacroCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroMacroCounterApp.swift
+//  MicroMacroCounter
+//
+//  Created by Indiana Huey on 5/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MicroMacroCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
